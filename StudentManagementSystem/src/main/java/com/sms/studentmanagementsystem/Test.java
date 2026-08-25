b5d5874709ace88f4fdd5fbeafc5eeb3b19a5bb0
Kyawt Kyawt San <kyawtkyawtsan11@gmail.com>
2026-08-25 16:03:23 +0100

    update

--- StudentManagementSystem/src/main/java/com/sms/studentmanagementsystem/Test.java
+++ StudentManagementSystem/src/main/java/com/sms/studentmanagementsystem/Test.java
@@ -12,7 +12,6 @@ public class Test {
     public static void main(String args []){
         System.out.println("Sweet world");
         System.out.println("Hello world");
-
         System.out.println("Stage");
 
     }
