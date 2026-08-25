4b756fe9212de79c82db229e09cf50b0adae5fa9
ZawHeinMg <zawheinmaung@mail.arakkha.net>
2026-08-25 09:51:31 +0630

    change name from zawheinmg

--- StudentManagementSystem/src/main/java/com/sms/studentmanagementsystem/StudentManagementSystem.java
+++ StudentManagementSystem/src/main/java/com/sms/studentmanagementsystem/StudentManagementSystem.java
@@ -12,6 +12,6 @@ public class StudentManagementSystem {
 
     public static void main(String[] args) {
         System.out.println("hi World!");
-        System.out.println("From ZHM");
+        System.out.println("From ZawHeinMg");
     }
 }
