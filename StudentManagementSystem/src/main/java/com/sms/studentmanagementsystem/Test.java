06ee6cc867ba86a4eee42ecfb6962dd2bade1a3c
U Zaw Moe <zawmoe123@gmail.com>
2026-08-25 22:16:58 +0630

    new line

--- StudentManagementSystem/src/main/java/com/sms/studentmanagementsystem/Test.java
+++ StudentManagementSystem/src/main/java/com/sms/studentmanagementsystem/Test.java
@@ -13,6 +13,7 @@ public class Test {
         System.out.println("Sweet world");
         System.out.println("Hello world");
         System.out.println("Hello Everyone");
+        System.out.println("Hi sir");
         
         
         
