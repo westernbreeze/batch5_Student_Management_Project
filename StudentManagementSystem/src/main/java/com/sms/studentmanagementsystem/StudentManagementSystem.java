ab11bb83ea1f762395af2691ad57c32e2c4d5a41
Zuu May Myint <zuumay123@gmail.com>
2026-08-25 22:15:04 +0530

    Update

--- StudentManagementSystem/src/main/java/com/sms/studentmanagementsystem/StudentManagementSystem.java
+++ StudentManagementSystem/src/main/java/com/sms/studentmanagementsystem/StudentManagementSystem.java
@@ -14,6 +14,7 @@ public class StudentManagementSystem {
         System.out.println("hi World!");
         System.out.println("From ZawHeinMaung");
         System.out.println("Hello");
+        System.out.println("Hello Everyone");
         //add new line from zhm
         //add new line from uzm
     }
