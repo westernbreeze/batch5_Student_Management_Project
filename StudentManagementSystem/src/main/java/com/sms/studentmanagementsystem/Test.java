6d7f19d509b314a4b4737017af7ba34d3c5251d4
U Zaw Moe <zawmoe123@gmail.com>
2026-08-25 21:54:57 +0630

    change stage to hello everyone from uzm

--- StudentManagementSystem/src/main/java/com/sms/studentmanagementsystem/Test.java
+++ StudentManagementSystem/src/main/java/com/sms/studentmanagementsystem/Test.java
@@ -12,7 +12,7 @@ public class Test {
     public static void main(String args []){
         System.out.println("Sweet world");
         System.out.println("Hello world");
-        System.out.println("Stage");
+        System.out.println("Hello Everyone");
 
     }
 }
