b8805f17da77cb0b37d9159764f11b770c5ac216
ZawHeinMg <zawheinmaung@mail.arakkha.net>
2026-08-26 16:27:04 +0630

    change my name

--- StudentManagementSystem/src/main/java/com/sms/studentmanagementsystem/Test.java
+++ StudentManagementSystem/src/main/java/com/sms/studentmanagementsystem/Test.java
@@ -35,7 +35,7 @@ public class Test {
         System.out.println("MgMg");
 
         System.out.println("Hello Everyone");
-        System.out.println("Hello I'm UserB");
+        System.out.println("Hello I'm UserA");
 
 
 }
